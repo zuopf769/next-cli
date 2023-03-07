2e3fca9f94f12c6a1b2b75ba58217cd141ff5b02
pengfei.zuo <pengfei.zuo@retech.work>
2023-03-07 20:05:12 +0800

    ConfigTransform 类的实现

---
+++ test.js
@@ -0,0 +1,10 @@
+// 本块代码仅用于测试
+const ConfigTransform = require('./lib/ConfigTransform.js')
+
+const vueConfig = new ConfigTransform({
+  js: ['vue.config.js']
+})
+
+console.log(vueConfig.transform({
+  text: '这是一个测试',
+}));
